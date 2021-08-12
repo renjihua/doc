ee1f9cdae1d5909942bbcb937efa376b8b4ca114
loltimor <r000000>
2021-08-12 16:13:13 +0800

    1

--- deploy.sh
+++ deploy.sh
@@ -9,17 +9,10 @@ npm run docs:build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
+git push -f https://github.com/renjihua/renjihua.github.io.git master
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
